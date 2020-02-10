959aff8ac190d9475aed09c93b584d7da24f4a66
sebastian cordoba <cbascordoba14@gmail.com>
2020-02-10 18:59:38 -0500

    6. Trabajando con callbacks para hacer 3 peticiones una seguida de la otra

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -2,23 +2,24 @@
 // guardo en la variable el valor del archivo XMLHttpRequest
 let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+let API= 'https://rickandmortyapi.com/api/character/';
 // funcion que nos permite traer la informacion desde nuestra API, recibe un callback y desencadena los llamados que necesitamos.
-function fetchData (url_api, callback){
+function fetchData(url_api, callback){
     // construimos la peticion por xmlhttprequest generando la referencia al objeto que necesito
     let xhttp = new XMLHttpRequest();
 // hacemos el llamado a una url 
-    xhttp.open('GET','url_api',true);/* el true activa el asincronismo  */
+    xhttp.open('GET',url_api,true);/* el true activa el asincronismo  */
      // escucho lo que hace la conexion 
     xhttp.onreadystatechange = function (event){
         // validamos para saber si fue exitoso el estado de la peticion
-        if(xhttp.readyState ===4){
+        if(xhttp.readyState === 4){
             //validamos el estado html si es exitoso
             if(xhttp.status === 200){
                 // regresamos el callback si todo sale bien, responseText me lo convierte de object a string
-                callback(null, JSON.parse(xhttp.responseText))
+                callback(null, JSON.parse(xhttp.responseText));
             }
             else{
-                const error = new Error('Error '+ url_api);
+                const error = new Error('Error aqui '+ url_api);
                 return callback(error, null);
             }
         }
@@ -26,3 +27,16 @@ function fetchData (url_api, callback){
 
     xhttp.send();
 }
+
+fetchData(API, function (error1, data1){
+    if (error1) return console.error(error1);
+    fetchData(API + data1.results[0].id, function (error2, data2) {
+        if (error2) return console.error(error2);
+        fetchData(data2.origin.url, function (error3, data3) {
+            if (error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })
+})
